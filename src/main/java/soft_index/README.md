8278b8aa82a0f747446b2dd3493a827944ae272c
rublin <toor.ua@gmail.com>
2017-09-23 15:49:34 +0300

    add comment and readme

--- src/main/java/soft_index/README.md
+++ src/main/java/soft_index/README.md
@@ -1,3 +1,11 @@
+# Information
+
+Implementation of **HashMap** for Integer as key and Long as value.
+
+Uses [open addressing strategy](https://en.wikipedia.org/wiki/Open_addressing).
+
+## Task description
+
 Напишите простую, эффективную реализацию алгоритма *Hash Map* с **открытой адресацией**, для ключей типа int и значений типа long. 
 
 Реализуйте операции put, get, size (remove не требуется, поддержка стандартного интерфейса Map не требуется).
--- src/main/java/soft_index/map/CustomMap.java
+++ src/main/java/soft_index/map/CustomMap.java
@@ -1,10 +1,33 @@
 package soft_index.map;
 
+/**
+ * Custom HashMap with open addressing strategy
+ */
 public interface CustomMap {
 
+    /**
+     * Associates the specified {@link Long} value with the specified {@link Integer} key in this map.
+     * If the map previously contained a mapping for the key, the old
+     * value is replaced.
+     * @param key key with which the specified value is to be associated
+     * @param value value to be associated with the specified key
+     * @return the previous value associated with <tt>key</tt>, or
+     *         <tt>null</tt> if there was no mapping for <tt>key</tt>.
+     *         (A <tt>null</tt> return can also indicate that the map
+     *         previously associated <tt>null</tt> with <tt>key</tt>.)
+     */
     Long put(int key, long value);
 
+    /**
+     * Returns the value to which the specified key is mapped,
+     * or {@code null} if this map contains no mapping for the key.
+     */
     Long get(int key);
 
+    /**
+     * Returns the number of key-value mappings in this map.
+     *
+     * @return the number of key-value mappings in this map
+     */
     int size();
 }
